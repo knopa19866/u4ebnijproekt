bd3fac7ab86f5531d969d0b1e6a669f2aaaf72ae
Anna Linenko <misija@mail.ru>
2022-12-13 04:12:08 +0100

    TicketsTests

---
+++ src/test/java/TicketsTests.java
@@ -0,0 +1,65 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class TicketsTests {
+    private final By FROM = By.id("afrom");
+    private final By TO = By.id("bfrom");
+    private final By GO_BTN = By.xpath(".//span[@class = 'gogogo']");
+
+    private final By FIRST_NAME = By.id("name");
+    private final By LAST_NAME = By.id("surname");
+    private final By DISCOUNT = By.id("discount");
+    private final By ADULTS = By.id("adults");
+    private final By CHILDREN = By.id("children");
+    private final By BAG = By.id("bugs");
+    private final By FLIGHT = By.id("flight");
+
+    private WebDriver browser;
+    private WebDriverWait wait;
+
+    @Test
+    public void reservationCheck() {
+        System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
+        browser = new ChromeDriver();
+        browser.manage().window().maximize();
+        browser.get("http://www.qaguru.lv:8089/tickets/");
+
+        wait = new WebDriverWait(browser, Duration.ofSeconds(10));
+
+        select(FROM, "RIX");
+        select(TO, "SFO");
+
+        browser.findElement(GO_BTN).click();
+
+        type(FIRST_NAME, "Anna");
+        type(LAST_NAME, "Markova");
+        type(DISCOUNT, "123456789");
+        type(ADULTS, "3");
+        type(CHILDREN, "2");
+        type(BAG, "1");
+        select(FLIGHT, "11");
+
+
+    }
+
+    private void select(By locator, String value) {
+        WebElement we = wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+        Select select = new Select(we);
+        select.selectByValue(value);
+    }
+
+    private void type(By locator, String text) {
+        WebElement input = wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+        input.clear();
+        input.sendKeys(text);
+    }
+}
